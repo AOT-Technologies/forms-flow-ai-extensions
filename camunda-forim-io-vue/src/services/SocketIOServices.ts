42179dfec7d3423fcff480ac0e0cd52fc6cacf4e
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-05-10 20:32:44 +0530

    taskEmail feature PR

--- camunda-forim-io-vue/src/services/SocketIOServices.ts
+++ camunda-forim-io-vue/src/services/SocketIOServices.ts
@@ -1,17 +1,16 @@
-import AES from 'crypto-js/aes';
 import SockJS from 'sockjs-client'
 import Stomp from 'stompjs'
+import AES from 'crypto-js/aes';
 
 let stompClient: any = null;
 const engine = "/engine-rest";
 const socketUrl = "/forms-flow-bpm-socket";
 const BPM_BASE_URL_SOCKET_IO = localStorage.getItem('bpmApiUrl') ? localStorage.getItem('bpmApiUrl')?.replace(engine, socketUrl) : ''
 const token: any = localStorage.getItem('authToken');
-// const WEBSOCKET_ENCRYPT_KEY: any = localStorage.getItem('websocketEncryptkey');
+const WEBSOCKET_ENCRYPT_KEY: any = localStorage.getItem('websocketEncryptkey');
 
 
 const isConnected = ()=>{
-  console.warn("Connected")
   return stompClient?.connected||null;
 };
 
@@ -20,7 +19,7 @@ const connect = (encryptKey: any, reloadCallback: any)=>{
   const socketUrl=`${BPM_BASE_URL_SOCKET_IO}?accesstoken=${accessToken}`;
   const socket = new SockJS(socketUrl);
   stompClient = Stomp.over(socket);
-  stompClient.connect({}, function(){
+  stompClient.connect({}, function(frame: any){
     if(isConnected()){
       stompClient.subscribe('/topic/task-event', function(output: any){
         const taskUpdate = JSON.parse(output.body);
@@ -31,7 +30,6 @@ const connect = (encryptKey: any, reloadCallback: any)=>{
 }
 
 const disconnect = ()=>{
-  console.warn("Disconnected")
   stompClient.disconnect();
 }
 
