c7baefae80d3bb39e0f252de8c28c85a24e9d5a6
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-05-18 18:54:51 +0530

    initialize mixins

---
+++ camunda-formio-tasklist-vue/src/mixins/BaseMixin.vue
@@ -0,0 +1,22 @@
+<script lang='ts'>
+import {Prop, Vue, Watch} from 'vue-property-decorator'
+
+export default class BaseMixin extends Vue {
+    @Prop() private bpmApiUrl!: string;
+    @Prop() private token!: string;
+    @Prop() private formIOResourceId!: string;
+    @Prop() private formIOReviewerId!: string;
+    @Prop() private formIOReviewer!: string;
+    @Prop() private formIOApiUrl!: string;
+    @Prop() private formsflowaiApiUrl!: string;
+    @Prop() private formsflowaiUrl!: string;
+    @Prop() private formIOUserRoles!: string;
+    @Prop() private userName!: string;
+    @Prop({default:'formflowai'}) private webSocketEncryptkey !: string;
+
+    @Watch('token')
+    ontokenChange (newVal: string) {
+      localStorage.setItem("authToken", newVal);
+    }
+}
+</script>
---
+++ camunda-formio-tasklist-vue/src/mixins/TaskListMixin.vue
@@ -0,0 +1,11 @@
+<script lang='ts'>
+
+import BaseMixin from './BaseMixin.vue';
+
+export default class TaskListMixin extends BaseMixin {
+
+    private task: any;
+    private test = 'hello';
+    
+}
+</script>
