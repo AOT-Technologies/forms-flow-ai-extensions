41de157c669145de7cb425ea32ef09c9df2626d7
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-05-19 17:49:02 +0530

    add breaking changes

--- camunda-formio-tasklist-vue/src/components/TaskList.vue
+++ camunda-formio-tasklist-vue/src/components/TaskList.vue
@@ -232,32 +232,31 @@ import 'semantic-ui-css/semantic.min.css';
 import '../styles/user-styles.css'
 import '../styles/camundaFormIOTasklist.scss'
 import { Component, Mixins, Prop } from 'vue-property-decorator'
-import {
-  TASK_FILTER_LIST_DEFAULT_PARAM,
-  findFilterKeyOfAllTask,
-  getTaskFromList,
-  getUserName
-} from '../services/utils';
+// import {
+//   TASK_FILTER_LIST_DEFAULT_PARAM,
+//   findFilterKeyOfAllTask,
+//   getTaskFromList,
+//   getUserName
+// } from '../services/utils';
 import BaseMixin from './mixins/BaseMixin.vue';
 import BpmnViewer from 'bpmn-js';
-import CamundaRest from '../services/camunda-rest';
-import DatePicker from 'vue2-datepicker'
+// import CamundaRest from '../services/camunda-rest';
+// import DatePicker from 'vue2-datepicker'
 import ExpandContract from './addons/ExpandContract.vue'
-import { Form } from 'vue-formio';
+// import { Form } from 'vue-formio';
 import Header from './layout/Header.vue'
 import LeftSider from './layout/LeftSider.vue'
-import {Payload} from '../services/TasklistTypes';
-import SocketIOService from '../services/SocketIOServices';
+// import {Payload} from '../services/TasklistTypes';
+// import SocketIOService from '../services/SocketIOServices';
 import TaskHistory from '../components/TaskHistory.vue';
-import {authenticateFormio} from '../services/formio-token';
-import {getFormDetails} from '../services/get-formio';
-import {getISODateTime} from '../services/format-time';
-import {getformHistoryApi} from '../services/formsflowai-api';
-import moment from 'moment';
-import { namespace } from 'vuex-class'
-import vSelect from 'vue-select'
+// import {authenticateFormio} from '../services/formio-token';
+// import {getFormDetails} from '../services/get-formio';
+// import {getISODateTime} from '../services/format-time';
+// import {getformHistoryApi} from '../services/formsflowai-api';
+// import moment from 'moment';
+// import { namespace } from 'vuex-class'
+// import vSelect from 'vue-select'
 
-const serviceFlowModule = namespace('serviceFlowModule')
 
 
 @Component({
@@ -272,7 +271,7 @@ const serviceFlowModule = namespace('serviceFlowModule')
     BpmnViewer
   },
 })
-export default class Tasklist extends Mixins(BaseMixin) {
+export default class Tasklist extends Mixins(BaseMixin, TaskListMixin) {
   @Prop() private getTaskId!: string;
 
   @serviceFlowModule.Getter('getFormsFlowTaskCurrentPage') private getFormsFlowTaskCurrentPage: any;
