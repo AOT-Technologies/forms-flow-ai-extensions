1039efabba45d982b3476547f6cae30dfe215d33
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-05-19 13:23:04 +0530

    update BaseMixin

--- camunda-formio-tasklist-vue/src/components/mixins/BaseMixin.vue
+++ camunda-formio-tasklist-vue/src/components/mixins/BaseMixin.vue
@@ -5,15 +5,15 @@ import {Component, Prop, Vue, Watch} from 'vue-property-decorator'
 export default class BaseMixin extends Vue {
     @Prop() protected readonly bpmApiUrl!: string;
     @Prop() protected readonly token!: string;
-    @Prop() private formIOResourceId!: string;
-    @Prop() private formIOReviewerId!: string;
-    @Prop() private formIOReviewer!: string;
-    @Prop() private formIOApiUrl!: string;
-    @Prop() private formsflowaiApiUrl!: string;
-    @Prop() private formsflowaiUrl!: string;
-    @Prop() private formIOUserRoles!: string;
-    @Prop() private userName!: string;
-    @Prop({default:'formflowai'}) private webSocketEncryptkey !: string;
+    @Prop() protected readonly formIOResourceId!: string;
+    @Prop() protected readonly formIOReviewerId!: string;
+    @Prop() protected readonly formIOReviewer!: string;
+    @Prop() protected readonly formIOApiUrl!: string;
+    @Prop() protected readonly formsflowaiApiUrl!: string;
+    @Prop() protected readonly formsflowaiUrl!: string;
+    @Prop() protected readonly formIOUserRoles!: string;
+    // @Prop() protected readonly userName!: string;
+    @Prop({default:'formflowai'}) public webSocketEncryptkey !: string;
 
     @Watch('token')
     ontokenChange (newVal: string) {
