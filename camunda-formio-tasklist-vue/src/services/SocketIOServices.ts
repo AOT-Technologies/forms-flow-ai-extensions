021c8652959617ce0af61a5e268ada08e0d7bd4f
Ann <ann.jacob@aot-technologies.com>
2021-06-21 22:40:06 -0400

    new websocket logic

--- camunda-formio-tasklist-vue/src/services/SocketIOServices.ts
+++ camunda-formio-tasklist-vue/src/services/SocketIOServices.ts
@@ -1,36 +1,73 @@
-import { engine, socketUrl } from "../services/constants";
+import { engine, socketUrl } from "./constants";
 import AES from "crypto-js/aes";
+import { Client } from "@stomp/stompjs";
 import SockJS from "sockjs-client";
-import Stomp from "stompjs";
 
-let stompClient: any = null;
 const BPM_BASE_URL_SOCKET_IO = localStorage.getItem("bpmApiUrl")
   ? localStorage.getItem("bpmApiUrl")?.replace(`/${engine}`, `/${socketUrl}`)
   : "";
-const token: any = localStorage.getItem("authToken");
+let websocketUrl: string = "";
+const token: string = localStorage.getItem("authToken") ?? "";
 
-const isConnected = () => {
-  return stompClient?.connected || null;
+const prepareUrl = (url: string) => {
+  const a = document.createElement("a");
+  a.href = url;
+  /* eslint-disable no-debugger, no-console */
+  if (a.protocol === "https:") return url.replace("https://", "wss://");
+  else return url.replace("http://", "ws://");
 };
-
-const connect = (encryptKey: any, reloadCallback: any) => {
-  const accessToken = AES.encrypt(token, encryptKey).toString();
-  const socketUrl = `${BPM_BASE_URL_SOCKET_IO}?accesstoken=${accessToken}`;
-  const socket = new SockJS(socketUrl);
-  stompClient = Stomp.over(socket);
-  stompClient.debug = null;
-  stompClient.connect({}, function () {
-    if (isConnected()) {
-      stompClient.subscribe("/topic/task-event", function (output: any) {
-        const taskUpdate = JSON.parse(output.body);
-        reloadCallback(taskUpdate.id, taskUpdate?.eventName);
-      });
-    }
+const stompClient = () => {
+  return new Client({
+    brokerURL: websocketUrl,
+    connectHeaders: {},
+    debug: function(str: string) {
+      console.log(str);
+    },
+    reconnectDelay: 5000,
+    heartbeatIncoming: 4000,
+    heartbeatOutgoing: 4000,
   });
 };
 
+let client = stompClient();
+
+const isConnected = () => {
+  return client.connected;
+};
+
+const connect = (encryptKey: string, reloadCallback: any) => {
+  const accessToken = AES.encrypt(token, encryptKey).toString();
+  const socketUrl = `${BPM_BASE_URL_SOCKET_IO}?accesstoken=${accessToken}`;
+  const socket = new SockJS(socketUrl);
+  console.log(socket.url, "++sockett");
+  websocketUrl = prepareUrl(socket.url);
+  console.log(
+    new WebSocket(websocketUrl),
+    "+++++++++++++++++++++@@@@@@@@@@@@@@@@@"
+  );
+  client = stompClient();
+  client.activate();
+  client.onConnect = function(frame: any) {
+    // Do something, all subscribes must be done is this callback
+    // This is needed because this will be executed after a (re)connect
+    client.subscribe("/topic/task-event", function(output: any) {
+      const taskUpdate = JSON.parse(output.body);
+      reloadCallback(taskUpdate.id, taskUpdate?.eventName);
+    });
+  };
+
+  client.onStompError = function(frame) {
+    // Will be invoked in case of error encountered at Broker
+    // Bad login/passcode typically will cause an error
+    // Complaint brokers will set `message` header with a brief message. Body may contain details.
+    // Compliant brokers will terminate the connection after any error
+    console.log("Broker reported error: " + frame.headers["message"]);
+    console.log("Additional details: " + frame.body);
+  };
+};
+
 const disconnect = () => {
-  stompClient.disconnect();
+  client.deactivate();
 };
 
 const SocketIOService = {
