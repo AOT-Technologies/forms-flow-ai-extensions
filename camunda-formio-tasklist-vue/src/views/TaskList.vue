8395a7c7e66e0d2bda70eb8c7a839cff4d4bdad8
Ann Mary Jacob <77353155+ann-aot@users.noreply.github.com>
2021-06-25 12:23:21 -0400

    [web][Websockets - Vue] Reconnect logic to be implemented (#49)
    [web][Websockets - Vue] Reconnect logic to be implemented

--- camunda-formio-tasklist-vue/src/views/TaskList.vue
+++ camunda-formio-tasklist-vue/src/views/TaskList.vue
@@ -3,13 +3,13 @@
     <CamundaTasklist
       :bpmApiUrl="configs.BPM_URL"
       :token="configs.token"
-      :formIOApiUrl= "configs.FORM_IO_API_URL"
-      :formIOResourceId = "configs.FORM_IO_RESOURCE_ID"
-      :formIOReviewerId = "configs.FORM_IO_REVIEWER_ID"
-      :formIOReviewer = "configs.FORM_IO_REVIEWER"
+      :formIOApiUrl="configs.FORM_IO_API_URL"
+      :formIOResourceId="configs.FORM_IO_RESOURCE_ID"
+      :formIOReviewerId="configs.FORM_IO_REVIEWER_ID"
+      :formIOReviewer="configs.FORM_IO_REVIEWER"
       :formsflowaiUrl="configs.FORM_FLOW_URL"
       :formsflowaiApiUrl="configs.FORM_FLOW_API_URL"
-      :formIOUserRoles=configs.FORMIO_ROLES
+      :formIOUserRoles="configs.FORMIO_ROLES"
       :getTaskId="getTaskId"
       v-if="isServiceFLowEnabled"
     />
@@ -18,40 +18,40 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
-import CamundaTasklist from '@/components/TaskList.vue'
+import { Component, Prop, Vue } from "vue-property-decorator";
+import CamundaTasklist from "@/components/TaskList.vue";
 // import configMap from '../utils/config-helper'
 @Component({
   components: {
-    CamundaTasklist
-  }
+    CamundaTasklist,
+  },
 })
 export default class TaskList extends Vue {
   @Prop() private getTaskId!: string;
-  public  configs = {
-    "token": "Bearer Token",
-    "BPM_URL": "Set Camunda URL",
-    "FORM_IO_API_URL": "Set FormIO Project URL",
-    "FORM_IO_RESOURCE_ID": "Set Formio Resource ID",
-    "FORM_IO_REVIEWER_ID": "Set Formio Reviewer ID",
-    "FORM_IO_REVIEWER": "Set Formio Reviewer Name",
-    "FORM_FLOW_API_URL":"Set formsflow.ai API URL",
-    "FORM_FLOW_URL":"Set formsflow.ai Project URL",
-    "SERVICEFLOW_ENABLED": true,
-    "FORMIO_ROLES" : "Set FORMIO User ROLES"
-  }
+  public configs = {
+    token: "Bearer Token",
+    BPM_URL: "Set Camunda URL",
+    FORM_IO_API_URL: "Set FormIO Project URL",
+    FORM_IO_RESOURCE_ID: "Set Formio Resource ID",
+    FORM_IO_REVIEWER_ID: "Set Formio Reviewer ID",
+    FORM_IO_REVIEWER: "Set Formio Reviewer Name",
+    FORM_FLOW_API_URL: "Set formsflow.ai API URL",
+    FORM_FLOW_URL: "Set formsflow.ai Project URL",
+    SERVICEFLOW_ENABLED: true,
+    FORMIO_ROLES: "Set FORMIO User ROLES",
+  };
 
-    public isServiceFLowEnabled = true
-    mounted () {
-      this.isServiceFLowEnabled = true
-    }
+  public isServiceFLowEnabled = true;
+  mounted() {
+    this.isServiceFLowEnabled = true;
+  }
 }
 </script>
 
 <style scoped>
 .no-content {
-    display: flex;
-    justify-content: center;
-    font-size: 18px;
+  display: flex;
+  justify-content: center;
+  font-size: 18px;
 }
 </style>
