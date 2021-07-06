4effc23ec7a7e7b3a54cf677e16d681f15905dc8
Ann <ann.jacob@aot-technologies.com>
2021-07-06 09:51:48 -0400

    store update

--- camunda-formio-tasklist-vue/src/store/index.ts
+++ camunda-formio-tasklist-vue/src/store/index.ts
@@ -1,5 +1,8 @@
-import  serviceFlowModule from './modules/serviceFlow-module'
+import serviceFlowModule from "./modules/serviceFlow-module";
 
-export default function install (Vue: any, options: any) {
-  options.store.registerModule('serviceFlowModule', serviceFlowModule)
+// export default function install (Vue: any, options: any) {
+//   options.store.registerModule('serviceFlowModule', serviceFlowModule)
+// }
+export default function install(store: any) {
+  store.registerModule("serviceFlowModule", serviceFlowModule);
 }
