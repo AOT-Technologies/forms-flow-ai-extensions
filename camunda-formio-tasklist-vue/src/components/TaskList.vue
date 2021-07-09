c3e71604c74114d4043c702e95a12263c09c3c05
Ann Mary Jacob <77353155+ann-aot@users.noreply.github.com>
2021-07-09 11:25:28 -0400

    [web][Store- Vue] Dynamic store registration (#58)
    * store update

--- camunda-formio-tasklist-vue/src/components/TaskList.vue
+++ camunda-formio-tasklist-vue/src/components/TaskList.vue
@@ -329,9 +329,10 @@ import { getformHistoryApi } from "../services/formsflowai-api";
 import moment from "moment";
 import { namespace } from "vuex-class";
 import { reviewerGroup } from "../services/constants";
+import serviceFlowModule from "../store/modules/serviceFlow-module";
 import vSelect from "vue-select";
 
-const serviceFlowModule = namespace("serviceFlowModule");
+const StoreServiceFlowModule = namespace("serviceFlowModule");
 
 @Component({
   components: {
@@ -349,18 +350,18 @@ export default class Tasklist extends Mixins(TaskListMixin) {
   @Prop() private getTaskId!: string;
   @Prop({ default: "lastName" }) userListType!: string;
 
-  @serviceFlowModule.Getter("getFormsFlowTaskCurrentPage")
+  @StoreServiceFlowModule.Getter("getFormsFlowTaskCurrentPage")
   private getFormsFlowTaskCurrentPage: any;
-  @serviceFlowModule.Getter("getFormsFlowTaskId")
+  @StoreServiceFlowModule.Getter("getFormsFlowTaskId")
   private getFormsFlowTaskId: any;
-  @serviceFlowModule.Getter("getFormsFlowactiveIndex")
+  @StoreServiceFlowModule.Getter("getFormsFlowactiveIndex")
   private getFormsFlowactiveIndex: any;
 
-  @serviceFlowModule.Mutation("setFormsFlowTaskCurrentPage")
+  @StoreServiceFlowModule.Mutation("setFormsFlowTaskCurrentPage")
   public setFormsFlowTaskCurrentPage: any;
-  @serviceFlowModule.Mutation("setFormsFlowTaskId")
+  @StoreServiceFlowModule.Mutation("setFormsFlowTaskId")
   public setFormsFlowTaskId: any;
-  @serviceFlowModule.Mutation("setFormsFlowactiveIndex")
+  @StoreServiceFlowModule.Mutation("setFormsFlowactiveIndex")
   public setFormsFlowactiveIndex: any;
 
   private tasks: Array<object> = [];
@@ -944,6 +945,14 @@ export default class Tasklist extends Mixins(TaskListMixin) {
     this.$root.$off("call-fetchPaginatedTaskList");
     this.$root.$off("call-fetchTaskList");
     this.$root.$off("call-managerScreen");
+    if (this.$store.hasModule("serviceFlowModule")) {
+      this.$store.unregisterModule("serviceFlowModule");
+    }
+  }
+  beforeCreate() {
+    if (!this.$store.hasModule("serviceFlowModule")) {
+      this.$store.registerModule("serviceFlowModule", serviceFlowModule);
+    }
   }
 }
 </script>
