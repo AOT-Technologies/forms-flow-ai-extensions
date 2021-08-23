9ac0604b758518d4c107207292c9abb27b21552c
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-08-23 10:00:40 +0530

    Update filterListFormatterService.ts

--- camunda-formio-tasklist-vue/src/services/filterListFormatterService.ts
+++ camunda-formio-tasklist-vue/src/services/filterListFormatterService.ts
@@ -1,4 +1,4 @@
-const compareFn = (a: { properties: { priority: number; }; },b: { properties: { priority: number; }; }) => {
+const compareFn = (a: any,b: any) => {
   const priority1=a?.properties?.priority||0;
   const priority2=b?.properties?.priority||0;
   return priority1-priority2;
