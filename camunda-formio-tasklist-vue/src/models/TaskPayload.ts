6fd3a72b8f074bb386cea89c7516a4735d51f2a9
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-08-27 21:17:11 +0530

    update formList sorting changes

---
+++ camunda-formio-tasklist-vue/src/models/TaskPayload.ts
@@ -0,0 +1,34 @@
+export interface TaskPayload {
+  id?: string;
+  name?: string;
+  assignee?: string;
+  created?: string;
+  due?: string|null;
+  followUp?: string|null;
+  delegationState?: string;
+  description?: string;
+  executionId?: string;
+  owner?: string;
+  parentTaskId?: string;
+  priority?: string;
+  processDefinitionId?: string;
+  processInstanceId?: string;
+  taskDefinitionKey?: string;
+  caseExecutionId?: string;
+  caseInstanceId?: string;
+  caseDefinitionId?: string;
+  suspended?: string;
+  formKey?: string;
+  tenantId?: string;
+}
+
+export interface CustomEventPayload {
+  actionType: string;
+  component: Object;
+  type: string;
+}
+
+export interface UserSearchListLabelPayload {
+  searchType: string;
+  label: string;
+}
