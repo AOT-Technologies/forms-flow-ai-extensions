92dd5db0b9e4b5e4db53a7492d7f08414935e871
kurianbenoy-aot <kurian.benoy@aot-technologies.com>
2021-11-22 22:51:26 +0530

    Update SearchOptions.ts

--- camunda-formio-tasklist-vue/src/models/SearchOptions.ts
+++ camunda-formio-tasklist-vue/src/models/SearchOptions.ts
@@ -17,7 +17,7 @@ export interface VariablePayload {
 }
 
 export interface TaskVariablesPayload {
-    taskVariables?: VariablePayload[]
+    taskVariables?: VariablePayload[];
     processVariables?: VariablePayload[];
 }
 
