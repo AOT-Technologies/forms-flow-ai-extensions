6e42b41bb300a8bd8315a5c8dc3f4f413394aa77
Kurian Benoy <70306694+kurianbenoy-aot@users.noreply.github.com>
2022-01-25 16:52:17 +0530

    Update GetFormio.ts

--- camunda-formio-tasklist-vue/src/services/GetFormio.ts
+++ camunda-formio-tasklist-vue/src/services/GetFormio.ts
@@ -7,9 +7,10 @@ export const getFormDetails = (formioUrl: string, formIOProjectUrl: string) => {
     a.protocol + "//" + a.host,
     b.protocol + "//" + b.host
   );
-  const formArr = formioUrl.split("/");
-  const formId: string = formArr[4];
-  const submissionId: string = formArr[6];
+    // Regex Extracts formId from */form/formId/submission/*
+  const formId = formioUrl.match(/(?<=\/form\/)(.*)(?=\/submission)/)[0];
+  // Regex Extracts submissionId from */submission/submissionId
+  const submissionId = formioUrl.match(/(?<=\/submission\/)(.*)(?=)/)[0];
   return {
     formioUrl, formId, submissionId 
   };
