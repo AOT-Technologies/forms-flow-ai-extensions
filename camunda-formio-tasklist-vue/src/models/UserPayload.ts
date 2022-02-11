934f55debb762b30e9f093754741bc1d12593cd4
shuhaib-aot <shuhaib.s@aot-technologies.com>
2022-02-11 12:47:57 +0530

    updated remove null from last name

--- camunda-formio-tasklist-vue/src/models/UserPayload.ts
+++ camunda-formio-tasklist-vue/src/models/UserPayload.ts
@@ -23,7 +23,7 @@ export const UserListObject = (user: UserPayload) => {
   return {
     code: user.id,
     email: user.email!,
-    firstName: `${user.firstName} ${user.lastName}`,
-    lastName: `${user.firstName} ${user.lastName}`,
+    firstName: `${user.firstName}  ${user.lastName}`,
+    lastName: `${user.firstName} ${user.lastName===null?"":user.lastName}`,
   };
 };
