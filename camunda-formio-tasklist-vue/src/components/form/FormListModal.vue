27d3266bd0d862578fc857e249e9fda6ef0cc6a1
shuhaib-aot <shuhaib.s@aot-technologies.com>
2022-03-27 12:04:47 +0530

    updated when form submited

--- camunda-formio-tasklist-vue/src/components/form/FormListModal.vue
+++ camunda-formio-tasklist-vue/src/components/form/FormListModal.vue
@@ -119,7 +119,7 @@ import {
   CamundaRest, FORMLIST_FIELDS, formApplicationSubmit
 } from "../../services";
 import {
-  Component, Mixins
+  Component, Emit, Mixins
 } from "vue-property-decorator";
 import {
   CustomEventPayload,
@@ -201,7 +201,8 @@ export default class FormListModal extends Mixins(BaseMixin) {
       );
     }
     this.$bvModal.hide("modal-multi-2");
-    // this.$bvModal.show('modal-multi-1');
+     this.$bvModal.show('modal-multi-1');
+     this.$emit("update-task")
   }
 
   oncustomEventCallback = (customEvent: CustomEventPayload) => {
