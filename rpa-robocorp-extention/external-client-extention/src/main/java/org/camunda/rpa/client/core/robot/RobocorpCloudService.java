55194def50c1ca119fbc972c48e1cb0df8b1d060
sneha <sneha@aot-technolo.com>
2022-05-09 19:22:58 +0530

    cloud changes

--- rpa-robocorp-extention/external-client-extention/src/main/java/org/camunda/rpa/client/core/robot/RobocorpCloudService.java
+++ rpa-robocorp-extention/external-client-extention/src/main/java/org/camunda/rpa/client/core/robot/RobocorpCloudService.java
@@ -3,10 +3,7 @@ package org.camunda.rpa.client.core.robot;
 import org.camunda.rpa.client.data.entity.RobotHandlerAudit;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import org.springframework.beans.factory.annotation.Value;
-import org.apache.commons.io.FileUtils;
 import org.camunda.rpa.client.core.pipe.RobotDirectoryScanner;
 import org.camunda.rpa.client.data.RobotInput;
 import org.camunda.rpa.client.data.entity.RobotHandlerConfig;
@@ -17,10 +14,7 @@ import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.stereotype.Service;
-import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.List;
 import reactor.core.publisher.Mono;
 import org.springframework.http.*;
@@ -49,16 +43,16 @@ public class RobocorpCloudService implements IRobotService {
 	@Override
 	public boolean runRobot(List<RobotInput> robotInputs, RobotHandlerAudit audit) {
 		try {
-			RobotHandlerConfig config = audit.getHandlerConfig();
-			return startRobotProcess(robotInputs, config);
+			return invokeRobot(robotInputs, audit);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return false;
 	}
 
-	private boolean startRobotProcess(List<RobotInput> robotInputs, RobotHandlerConfig config) throws JSONException {
-		String response = invokeRobot(robotInputs, config);
+	private boolean invokeRobot(List<RobotInput> robotInputs, RobotHandlerAudit audit) throws JSONException {
+		RobotHandlerConfig config = audit.getHandlerConfig();
+		String response = startRobotProcess(robotInputs, config);
 		JSONObject jsonObject = new JSONObject(response);
 		String robotRunId = jsonObject.getString("id");
 		String workItemId = new JSONArray(jsonObject.getString("workItemIds")).getString(0);
@@ -76,7 +70,7 @@ public class RobocorpCloudService implements IRobotService {
 				}
 				success = isRobotCompleted(output);
 				if (success) {
-					collectResponseFile(output, workItemId, robotRunId, config);
+					setResponseUri(output, workItemId, config, robotInputs, audit);
 					break;
 				}
 			} catch (Exception e) {
@@ -86,7 +80,7 @@ public class RobocorpCloudService implements IRobotService {
 		return success;
 	}
 
-	private String invokeRobot(List<RobotInput> robotInputs, RobotHandlerConfig config) throws JSONException {
+	private String startRobotProcess(List<RobotInput> robotInputs, RobotHandlerConfig config) throws JSONException {
 		String formattedInput = buildInput(robotInputs).toString();
 		String uri = getRobotProcessUrl(config) + "/runs";
 		ResponseEntity<String> response = webClient.method(HttpMethod.POST).uri(uri)
@@ -115,8 +109,8 @@ public class RobocorpCloudService implements IRobotService {
 		return response.getBody();
 	}
 
-	private void collectResponseFile(String output, String workItemId, String robotRunId, RobotHandlerConfig config)
-			throws JSONException, IOException {
+	private void setResponseUri(String output, String workItemId, RobotHandlerConfig config,
+			List<RobotInput> robotInputs, RobotHandlerAudit audit) throws JSONException, IOException {
 		String jsonData = new JSONObject(output).getString("data");
 		String data = jsonData != null ? new JSONArray(jsonData).getString(0) : null;
 		String files = data != null ? new JSONObject(data).getString("files") : null;
@@ -125,25 +119,9 @@ public class RobocorpCloudService implements IRobotService {
 			JSONObject jsonObject = new JSONObject(fileIds);
 			String fileId = jsonObject.getString("id");
 			String fileName = jsonObject.getString("name");
-
 			String uri = getRobotProcessUrl(config) + "/work-items/" + workItemId + "/files/" + fileId + "/download";
-			ResponseEntity<String> response = webClient.method(HttpMethod.GET).uri(uri)
-					.header("Authorization", getApiKey()).accept(MediaType.APPLICATION_JSON)
-					.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).body(null).retrieve()
-					.toEntity(String.class).block();
-
-			LOG.error(response.getBody());
-			downloadResponseFile(response, fileId, fileName, config);
+			audit.setResponseUri(uri);
 		}
-	}
-
-	private void downloadResponseFile(ResponseEntity<String> response, String fileId, String fileName,
-			RobotHandlerConfig config) throws MalformedURLException, IOException, JSONException {
-		JSONObject jsonObject = new JSONObject(response.getBody());
-		String outputUrl = jsonObject.getString("url");
-		File outputDir = robotDirectoryScanner.getRobotFinalDirectory(config.getRobotName(),
-				config.getWorkingDirName());
-		FileUtils.copyURLToFile(new URL(outputUrl), new File(outputDir.getAbsolutePath() + File.separator + fileName));
 	}
 
 	private boolean isRobotCompleted(String result) {
