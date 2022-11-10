31e2fff532b89dcdc2201530f3cede68829089fe
shuhaib s <95394061+shuhaib-aot@users.noreply.github.com>
2022-11-10 19:59:29 +0530

    Drop down fix (#281)
    * fixed form list and submission
    
    * safari issue fix Regex issue
    
    * drop down issue fix and vue-formio package upgrade

--- camunda-formio-tasklist-vue/src/services/GetFormio.ts
+++ camunda-formio-tasklist-vue/src/services/GetFormio.ts
@@ -4,16 +4,15 @@ export const getFormDetails = (formioUrl: string, formIOProjectUrl: string) => {
   let formId: any;
   let submissionId: any;
   if (formioUrl) {
-    let formString = "/form/";
-    let submissionString = "/submission/";
-    let firstPositionOfString = formioUrl.indexOf("/form/");
-    let lastPositionOfString = formioUrl.indexOf("/submission");
+    const formString = "/form/";
+    const submissionString = "/submission/";
+    const firstPositionOfString = formioUrl.indexOf("/form/");
+    const lastPositionOfString = formioUrl.indexOf("/submission");
     formId = formioUrl.substring(
       firstPositionOfString + formString.length,
       lastPositionOfString
     );
-    let firstPositionOfSubmissionString =
-    formioUrl.indexOf(submissionString) + submissionString.length;
+    const firstPositionOfSubmissionString = formioUrl.indexOf(submissionString) + submissionString.length;
     submissionId = formioUrl.substring(firstPositionOfSubmissionString);
   }
   const a = document.createElement("a");
